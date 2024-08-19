00dd24fad9b1f83e428104c32fc8c6219fedaab6
Nihalroshan55 <nihalroshan55@gmail.com>
2024-08-19 15:03:41 +0530

    changed syntax

--- ant_tools/ant_tools/doctype/ant_tools_setting/ant_tools_setting.js
+++ ant_tools/ant_tools/doctype/ant_tools_setting/ant_tools_setting.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Anther and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Ant tools setting', {
+frappe.ui.form.on("Ant tools setting", {
 	// refresh: function(frm) {
 
 	// }
--- ant_tools/api/utils.py
+++ ant_tools/api/utils.py
@@ -1,19 +1,19 @@
-import frappe
-import barcode
-from barcode.writer import SVGWriter
 from io import BytesIO
+import barcode
+import frappe
+from barcode.writer import SVGWriter
 import base64
 from collections import defaultdict
 
 @frappe.whitelist(allow_guest=True)
 def barcode_generator(data):
-    '''
+    """
 
         This function will accept a param as data and return an svg data as barcode use a access it throw jinga tag as {{barcode_generator(data)}}
 
-    '''
+    """
     try:
-        code128 = barcode.get_barcode_class('code128')
+        code128 = barcode.get_barcode_class("code128")
 
         # Create the barcode instance
         barcode_instance = code128(data, writer=SVGWriter())
@@ -28,13 +28,13 @@ def barcode_generator(data):
         svg_stream.seek(0)
 
         # Read the SVG data from the stream
-        svg_data = svg_stream.read().decode('utf-8')
+        svg_data = svg_stream.read().decode("utf-8")
 
         # Encode the SVG data as base64
-        base64_svg_data = base64.b64encode(svg_data.encode('utf-8')).decode('utf-8')
+        base64_svg_data = base64.b64encode(svg_data.encode("utf-8")).decode("utf-8")
 
         # Construct the data URI
-        data_uri = f'data:image/svg+xml;base64,{base64_svg_data}'
+        data_uri = f"data:image/svg+xml;base64,{base64_svg_data}"
         return data_uri
     except:
         #   Throw a error message 
@@ -42,9 +42,9 @@ def barcode_generator(data):
 
 @frappe.whitelist(allow_guest=True)
 def length_counter(data):
-    '''
+    """
         This function will accept a variable ant return the length of it 
-    '''
+    """
     try:
         return len(data)
     except:
@@ -52,54 +52,51 @@ def length_counter(data):
 
 @frappe.whitelist(allow_guest=True)
 def item_gst_filter(items):
-    grouped_items = defaultdict(lambda: {
-        'total_sgst_amount': 0,
-        'total_cgst_amount': 0,
-        'total_igst_amount': 0,
-        'total_qty': 0,
-        'total_taxable_amount': 0,
-        'uom': None,
-        'hsn': None,
-        'igst_rate': None,
-        'cgst_rate': None,
-        'sgst_rate': None,
-        'items': []
-    })
+    grouped_items = defaultdict(
+        lambda: {
+            "total_sgst_amount": 0,
+            "total_cgst_amount": 0,
+            "total_igst_amount": 0,
+            "total_qty": 0,
+            "total_taxable_amount": 0,
+            "uom": None,
+            "hsn": None,
+            "igst_rate": None,
+            "cgst_rate": None,
+            "sgst_rate": None,
+            "items": []
+        }
+    )
 
     # Group items by gst_hsn_code, igst_rate, cgst_rate, and sgst_rate
     for item in items:
-        key = (
-            item.get('gst_hsn_code'),
-            item.get('igst_rate'),
-            item.get('cgst_rate'),
-            item.get('sgst_rate')
-        )
+        key = ( item.get("gst_hsn_code"), item.get("igst_rate"), item.get("cgst_rate"), item.get("sgst_rate") )
         
         # Add item to the group
-        grouped_items[key]['items'].append(item)
+        grouped_items[key]["items"].append(item)
         
         # Safely sum up SGST, CGST, and IGST amounts, treating None as 0
-        grouped_items[key]['total_sgst_amount'] += item.get('sgst_amount', 0) or 0
-        grouped_items[key]['total_cgst_amount'] += item.get('cgst_amount', 0,2) or 0
-        grouped_items[key]['total_igst_amount'] += item.get('igst_amount', 0,2) or 0
+        grouped_items[key]["total_sgst_amount"] += item.get("sgst_amount", 0) or 0
+        grouped_items[key]["total_cgst_amount"] += item.get("cgst_amount", 0,2) or 0
+        grouped_items[key]["total_igst_amount"] += item.get("igst_amount", 0,2) or 0
         
         # Sum the quantities and taxable amounts, treating None as 0
-        grouped_items[key]['total_qty'] += item.get('qty', 0) or 0
-        grouped_items[key]['total_taxable_amount'] += item.get('taxable_value', 0) or 0
+        grouped_items[key]["total_qty"] += item.get("qty", 0) or 0
+        grouped_items[key]["total_taxable_amount"] += item.get("taxable_value", 0) or 0
         
         # Set UOM and HSN (use the first item's UOM for each group)
-        if grouped_items[key]['uom'] is None:
-            grouped_items[key]['uom'] = item.get('uom')
-            grouped_items[key]['hsn'] = item.get('gst_hsn_code')
-            grouped_items[key]['igst_rate'] = item.get('igst_rate', 0) or 0
-            grouped_items[key]['cgst_rate'] = item.get('cgst_rate', 0) or 0
-            grouped_items[key]['sgst_rate'] = item.get('sgst_rate', 0,2) or 0
+        if grouped_items[key]["uom"] is None:
+            grouped_items[key]["uom"] = item.get("uom")
+            grouped_items[key]["hsn"] = item.get("gst_hsn_code")
+            grouped_items[key]["igst_rate"] = item.get("igst_rate", 0) or 0
+            grouped_items[key]["cgst_rate"] = item.get("cgst_rate", 0) or 0
+            grouped_items[key]["sgst_rate"] = item.get("sgst_rate", 0,2) or 0
 
     # Convert defaultdict to a list of dictionaries for easier use in Jinja
     grouped_list = []
     idx = 1
     for group, data in grouped_items.items():
-        data['idx'] = idx
+        data["idx"] = idx
         grouped_list.append(data)
         idx += 1
     return grouped_list
